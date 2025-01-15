4e9fe9d55b22c4be13d6207decf4dc43fc91e922
lihaibo <hfms_lhb@outlook.com>
2025-01-15 22:55:01 +0800

    test: 增加同一个界面多个区域跳转测试

--- tests/Underdog.Wpf.Tests/Common/RegionKey.cs
+++ tests/Underdog.Wpf.Tests/Common/RegionKey.cs
@@ -8,6 +8,9 @@ namespace Underdog.Wpf.Tests.Common
 {
     public static class RegionKey
     {
-        public const string Root = "ContentRegion";
+        public const string Root1 = "ContentRegion1";
+        public const string Root2 = "ContentRegion2";
+        public const string Root3 = "ContentRegion3";
+        public const string Root4 = "ContentRegion4";
     }
 }
--- tests/Underdog.Wpf.Tests/ViewModels/MainWindowViewModel.cs
+++ tests/Underdog.Wpf.Tests/ViewModels/MainWindowViewModel.cs
@@ -19,85 +19,104 @@ using Underdog.Wpf.Tests.Views;
 
 namespace Underdog.Wpf.Tests.ViewModels
 {
-    public partial class MainWindowViewModel:ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
         private readonly IDialogService _dialogService;
         private readonly IRegionManager _regionManager;
         private readonly IRegionViewScanner _regionViewScanner;
         private readonly IConfiguration _configuration;
         private readonly IServiceProvider _serviceProvider;
-        public MainWindowViewModel(IServiceProvider serviceProvider, IConfiguration configuration, IRegionViewScanner regionViewScanner, IDialogService dialogService,IRegionManager regionManager)
+        public MainWindowViewModel(IServiceProvider serviceProvider, IConfiguration configuration, IRegionViewScanner regionViewScanner, IDialogService dialogService, IRegionManager regionManager)
         {
             _serviceProvider = serviceProvider;
             _configuration = configuration;
             _dialogService = dialogService;
             _regionManager = regionManager;
             _regionViewScanner = regionViewScanner;
-
-            ShowDialog1Command = new RelayCommand(() =>
-            {
-                var message = "This is a message that should be shown in the dialog.";
-                //using the dialog service as-is
-                _dialogService.ShowDialog("NotificationDialog1", new DialogParameters($"message={message}"), r =>
-                {
-                    if (r.Result == ButtonResult.None)
-                        Title = "Result is None";
-                    else if (r.Result == ButtonResult.OK)
-                        Title = "Result is OK";
-                    else if (r.Result == ButtonResult.Cancel)
-                        Title = "Result is Cancel";
-                    else
-                        Title = "I Don't know what you did!?";
-                }, "MessageBoxC");
-            });
-
-            ShowDialog2Command = new RelayCommand(() =>
-            {
-                var message = "This is a message that should be shown in the dialog.";
-                //using the dialog service as-is
-                _dialogService.ShowDialog("NotificationDialog2", new DialogParameters($"windowName=MessageBoxC&message={message}"), r =>
-                {
-                    if (r.Result == ButtonResult.None)
-                        Title = "Result is None";
-                    else if (r.Result == ButtonResult.OK)
-                        Title = "Result is OK";
-                    else if (r.Result == ButtonResult.Cancel)
-                        Title = "Result is Cancel";
-                    else
-                        Title = "I Don't know what you did!?";
-                });
-            });
-
-            ShowRegion1Command = new(() =>
-            {
-                _regionManager.RequestNavigate(RegionKey.Root, _regionViewScanner.GetViewAssemblyQualifiedName<ViewAViewModel>());
-            });
-
-            ShowRegion2Command = new(() =>
-            {
-                _regionManager.RequestNavigate(RegionKey.Root, _regionViewScanner.GetViewAssemblyQualifiedName<ViewBViewModel>());
-            });
         }
-
-        public RelayCommand ShowDialog1Command { get; }
-        public RelayCommand ShowDialog2Command { get; }
-
-        public RelayCommand ShowRegion1Command { get; }
-        public RelayCommand ShowRegion2Command { get; }
 
         [ObservableProperty]
         private string title = "Welcome to Underdog WPF Tests";
 
         [RelayCommand]
+        private void ShowDialog1()
+        {
+            var message = "这是一个自定义window窗体的弹窗，通过参数传递窗体名称";
+            //using the dialog service as-is
+            _dialogService.ShowDialog("NotificationDialog1", new DialogParameters($"message={message}"), r =>
+            {
+                if (r.Result == ButtonResult.None)
+                    Title = "Result is None";
+                else if (r.Result == ButtonResult.OK)
+                    Title = "Result is OK";
+                else if (r.Result == ButtonResult.Cancel)
+                    Title = "Result is Cancel";
+                else
+                    Title = "I Don't know what you did!?";
+            }, "MessageBoxC");
+        }
+
+        [RelayCommand]
+        private void ShowDialog2() 
+        {
+            var message = "这是一个自定义window窗体的弹窗，通过DialogParameters传递窗体名称";
+            //using the dialog service as-is
+            _dialogService.ShowDialog("NotificationDialog2", new DialogParameters($"windowName=MessageBoxC&message={message}"), r =>
+            {
+                if (r.Result == ButtonResult.None)
+                    Title = "Result is None";
+                else if (r.Result == ButtonResult.OK)
+                    Title = "Result is OK";
+                else if (r.Result == ButtonResult.Cancel)
+                    Title = "Result is Cancel";
+                else
+                    Title = "I Don't know what you did!?";
+            });
+        }
+
+        [RelayCommand]
+        private void ShowRegion1()
+        {
+            _regionManager.RequestNavigate(RegionKey.Root1, _regionViewScanner.GetViewAssemblyQualifiedName<ViewAViewModel>());
+
+        }
+
+        [RelayCommand]
+        private void ShowRegion2()
+        {
+            _regionManager.RequestNavigate(RegionKey.Root2, _regionViewScanner.GetViewAssemblyQualifiedName<ViewBViewModel>());
+        }
+
+        [RelayCommand]
         private void ShowRegion3() 
         {
-            _regionManager.RequestNavigate(RegionKey.Root, _regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleA.ViewModels.ViewAViewModel>());
+            _regionManager.RequestNavigate(RegionKey.Root3, _regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleA.ViewModels.ViewAViewModel>());
         }
 
         [RelayCommand]
         private void ShowRegion4()
         {
-            _regionManager.RequestNavigate(RegionKey.Root, _regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleB.ViewModels.ViewAViewModel>());
+            _regionManager.RequestNavigate(RegionKey.Root4, _regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleB.ViewModels.ViewAViewModel>());
+        }
+
+        [RelayCommand]
+        private void ShowRegion5()
+        {
+            //_regionManager.RequestNavigate(RegionKey.Root3, _regionViewScanner.GetViewAssemblyQualifiedName<ViewAViewModel>());
+            //_regionManager.RequestNavigate(RegionKey.Root4, _regionViewScanner.GetViewAssemblyQualifiedName<ViewBViewModel>());
+
+            _regionManager.Regions[RegionKey.Root3].NavigationService.RequestNavigate(_regionViewScanner.GetViewAssemblyQualifiedName<ViewAViewModel>());
+            _regionManager.Regions[RegionKey.Root4].NavigationService.RequestNavigate(_regionViewScanner.GetViewAssemblyQualifiedName<ViewBViewModel>());
+        }
+
+        [RelayCommand]
+        private void ShowRegion6()
+        {
+            //_regionManager.RequestNavigate(RegionKey.Root3, _regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleA.ViewModels.ViewAViewModel>());
+            //_regionManager.RequestNavigate(RegionKey.Root4, _regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleB.ViewModels.ViewAViewModel>());
+
+            _regionManager.Regions[RegionKey.Root3].NavigationService.RequestNavigate(_regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleA.ViewModels.ViewAViewModel>());
+            _regionManager.Regions[RegionKey.Root4].NavigationService.RequestNavigate(_regionViewScanner.GetViewAssemblyQualifiedName<Underdog.Wpf.Tests.ModuleB.ViewModels.ViewAViewModel>());
         }
     }
 }
